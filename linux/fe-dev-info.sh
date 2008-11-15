f542ec4c268fb401ef5e151b1bc514db723d5de2
Brandon Philips <brandon@ifup.org>
2008-11-14 23:49:22 -0800

    fe-dev-info.sh: Fixes

--- linux/fe-dev-info.sh
+++ linux/fe-dev-info.sh
@@ -1,17 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 # Simple tool to get the information about the FEUSB device
 
-# Usage: sh info.sh [ /dev/ttyACM* | /dev/fercs* ]
+# Usage: fe-dev-info.sh [ /dev/ttyACM* | /dev/fercs* ]
 
 DEV=/dev/ttyACM0
 
-SEARCH="(idVendor|idProduct|bcdDevice|manufacturer|product|serial)"
-GETINFO="/sbin/udevadm info -a -p $(/sbin/udevadm info -q path -n /dev/ttyACM0)"
+if [ ! -c $DEV ]; then
+	exit 1
+fi
 
 if [ $# -gt 0 ]; then
 	DEV=$1
 	echo "Using DEV $1"
 fi
+
+SEARCH="(idVendor|idProduct|bcdDevice|manufacturer|product|serial)"
+GETINFO="/sbin/udevadm info -a -p $(/sbin/udevadm info -q path -n /dev/ttyACM0)"
+
 
 $GETINFO | egrep $SEARCH | head -n6
